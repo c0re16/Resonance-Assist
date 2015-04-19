225382ba50fc984b15f5a2186f7f3ec9e198f291
c0re16 <cgball13@gmail.com>
2015-04-19 14:57:26 -0400

    removed some stuff

--- hellomake.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <hellomake.h>
-int main(int argc, char *argv[]) {
-len = atoi(argv[1]);
-examplefunction(len);
-return(0);
-}
--- hellomake.h
+++
@@ -1,2 +0,0 @@
-int len;
-void examplefunction();
